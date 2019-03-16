ab39d3cccef05bcdff25bc221f02059f047e30d3
Preston Neal <pneal1995@gmail.com>
2019-03-16 09:37:26 -0500

    Basic Algorithms Solved in JS

---
+++ Beginner/capSentence/index-START.js
@@ -0,0 +1,32 @@
+/* CHALLENGE
+Given a sentence containing two or more words, 
+return the equivalent of the sentence when capitalised. E.g
+  capSentence('the tales of scotch!') // would return 'The Tales Of Scotch!' 
+*/
+
+
+
+
+function capSentence(text) {
+   let wordsArray = text.toLowerCase().split(' ');
+   let capsArray = [];
+
+   wordsArray.forEach(word => {
+     capsArray.push(word[0].toUpperCase() + word.slice(1));
+   })
+
+   return capsArrayArray.join(' ');
+}
+
+function capSentence(text) {
+  let wordsArray = text.toLowerCase().split(' ');
+
+  let capsArray = wordsArray.map(word => {
+    return word.replace(word[0], word[0].toUpperCase());
+  })
+
+  return capsArray.join(' ')
+}
+
+
+module.exports = capSentence
