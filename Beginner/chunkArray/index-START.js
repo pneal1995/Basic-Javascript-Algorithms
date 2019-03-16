62375532d81b9b91cc81e4b41ebdd669fb243b94
Preston Neal <pneal1995@gmail.com>
2019-03-16 11:04:02 -0500

    3/16

--- Beginner/chunkArray/index-START.js
+++ Beginner/chunkArray/index-START.js
@@ -4,6 +4,7 @@
  E.g  mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
 
+//LOOP THROUGH THE ARRAY
 function chunkArray(array, size) {
     let result = [];
 
@@ -20,12 +21,40 @@ function chunkArray(array, size) {
     return result
 }
 
+//LOOPING THROUGH THE NUMBER OF CHUNKS
+function chunkArray(array, size) {
+    let result = [];
+
+    let arrayCopy = [...array];
+
+    while (arrayCopy.length > 0) {
+        result.push(arrayCopy.splice(0, size));
+    }
+
+    return result
+}
+
+//USING .SLICE
+
+function chunkArray3(array, size) {
+    let result = [];
+
+    for (i = 0; i < array.length; i += size) {
+        let chunk = array.slice(i, i + size);
+        result.push(chunk);
+    }
+    return result
+    
+}
+
+
+//RECURSION
 function chunkArray(array, size) {
     if(array.length <= size){
         return [array]
     }
-    return [array.slice(0,size), ...chunkArray(array.slice)]
-}
+    return [array.slice(0,size), ...chunkArray(array.slice(size), size)]
+ }
 
 
 module.exports = chunkArray
